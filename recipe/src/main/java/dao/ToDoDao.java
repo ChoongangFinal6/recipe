69f5d2e77fea1a5b15e9d8c0fd60f68b3d177c15
Heeuk Kang <kheeuk@gmail.com>
2015-06-29 17:40:55 +0900

    aaa

--- recipe/src/main/java/dao/ToDoDao.java
+++ recipe/src/main/java/dao/ToDoDao.java
@@ -1,19 +1,6 @@
 package dao;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-
-import model.ToDo;
 
 public interface ToDoDao {
-	HashMap<Integer, List<ToDo>> startTotal(Calendar fDay);
-	HashMap<Integer, List<ToDo>> endTotal(Calendar fDay);
-	int insert(ToDo todo);
-	ToDo detail(String id);
-	int del(String id);
-	int modify(ToDo todo);
-	int toggle(String id);
-	int updateEndTime(ToDo todo);
-	int updateDuration(ToDo todo);
+
 }
---
+++ recipe/src/main/java/model/Rating.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.sql.Timestamp;
+
+
+public class Rating {
+	int no, postNo;
+	int rate;
+	String email;
+	Timestamp inputDate;
+}
