3ef2f9f9c4440f01e50c3cd0b3624497a0e3dc34
heeuk <kheeuk@gmail.com>
2015-07-05 23:21:15 +0900

    aaa

--- recipe/src/main/webapp/recipe.sql
+++ recipe/src/main/webapp/recipe.sql
@@ -22,7 +22,7 @@ insert into MATERIAL values (11, '두부', 'grain');
 insert into MATERIAL values (12, '간장', 'source');
 insert into MATERIAL values (13, '김치', 'vegetable');
 insert into MATERIAL values (14, '된장', 'source');
-insert into MATERIAL values (14, '물', 'water');
+insert into MATERIAL values (15, '물', 'water');
 
 
 
